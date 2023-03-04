b381dd962891d20c888c485ce8163c10559cd5ff
XsChen524 <43726972+XsChen524@users.noreply.github.com>
2023-03-04 21:47:17 +0800

    Update index.d.ts

--- src/services/index.d.ts
+++ src/services/index.d.ts
@@ -1,35 +1,45 @@
 export type NativeSignupForm = {
-  name: string;
-  email: string;
-  password: string;
+	name: string;
+	email: string;
+	password: string;
 };
 
 export type EggSignupReturn = {
-  status: boolean;
-  msg: string;
+	status: boolean;
+	msg: string;
 };
 
 export type SignUpReturn = {
-  responseBody: EggSignupReturn;
-  status: number;
+	responseBody: EggSignupReturn;
+	status: number;
+};
+
+export type NativeLoginForm = {
+	email: string;
+	password: string;
 };
 
 export type UserData = {
-  id: number;
-  email: string;
-  name: string;
-  token: string;
+	id: number | undefined;
+	email: string | undefined;
+	name: string | undefined;
+	token: string | undefined;
 };
 
 export type EggLoginReturn = {
-  status: boolean;
-  msg: string;
-  data: undefined | userData;
+	status: boolean;
+	msg: string;
+	data: undefined | UserData;
+};
+
+export type LoginReturn = {
+	status: number;
+	response: EggLoginReturn;
 };
 
 export type JwtDecodedTokenType = {
-  id: number;
-  email: string;
-  iat: number; // Timestamp in millsecond
-  exp: number; // Timestamp in millsecond
+	id: number;
+	email: string;
+	iat: number; // Timestamp in millsecond
+	exp: number; // Timestamp in millsecond
 };
