1726f0c09da3a4d1e7838cf7885987ae3a45d8b8
XsChen524 <43726972+XsChen524@users.noreply.github.com>
2023-03-06 00:14:11 +0800

    Add items path

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,16 +1,17 @@
 const config = {
-  env: {
-    dev: "http://192.168.1.110:7001",
-    pro: "https://mobile.tracechain.top",
-  },
-  url: {
-    index: "/",
-    auth: {
-      index: "/",
-      signup: "/auth/signup",
-      login: "/auth/login",
-    },
-  },
+	env: {
+		dev: "http://192.168.1.110:7001",
+		pro: "https://mobile.tracechain.top",
+	},
+	url: {
+		index: "/",
+		auth: {
+			index: "/",
+			signup: "/auth/signup",
+			login: "/auth/login",
+		},
+		item: "/items",
+	},
 };
 
 export default config;
