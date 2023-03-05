9fe7312276b557b3c447337c299884e3b341e5d0
XsChen524 <43726972+XsChen524@users.noreply.github.com>
2023-03-06 06:10:42 +0800

    Update OrderDetailScreen.tsx and OrderScreen.tsx

---
+++ src/screens/Order/OrderDetailScreen.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Box, NativeBaseProvider, Text } from "native-base";
+import { NativeStackScreenProps } from "@react-navigation/native-stack";
+import { OrderStackParamList } from "../../router";
+
+type OrderDetailProps = NativeStackScreenProps<OrderStackParamList, 'OrderDetailStack'>;
+
+const OrderDetailScreen: React.FunctionComponent<OrderDetailProps> = (props: OrderDetailProps) => {
+	console.log(props);
+	return (
+		<NativeBaseProvider>
+			<Box>
+				<Text>Details</Text>
+			</Box>
+		</NativeBaseProvider>
+	)
+}
+
+export default OrderDetailScreen;
--- src/screens/Order/OrderScreen.tsx
+++ src/screens/Order/OrderScreen.tsx
@@ -1,115 +1,179 @@
-import React from 'react'
-import { View, Text, NativeBaseProvider, ScrollView, Box, HStack, VStack, Icon, Button } from 'native-base';
+import React, { useEffect, useState } from 'react'
+import { Image, View, Text, NativeBaseProvider, ScrollView, Box, HStack, VStack, Button, Center, Spacer, FlatList, Icon } from 'native-base';
+import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import { useAppSelector } from '../../redux/hooks';
 import { selectAuthState } from '../../redux/AuthSlice';
 import { OrderStackParamList } from '../../router';
-import { NativeStackScreenProps } from '@react-navigation/native-stack';
-import { Image } from 'react-native';
-import Avatar from '../../components/Avatar';
-import { Ionicons, MaterialIcons } from "@expo/vector-icons";
-import { baseFontSize, isLight } from 'native-base/lib/typescript/theme/tools';
+import { Item } from '../../services/item';
 import { getAllItemSync } from '../../services/item/item';
+import Avatar from '../../components/Avatar';
+import moment from 'moment';
+import { Ionicons } from "@expo/vector-icons";
 
 type OrderScreenProps = NativeStackScreenProps<OrderStackParamList, 'OrderStack'>;
 
 const Profile: React.FunctionComponent<
-	{ userName: string; userAvatarUrl: string; numItems: number } & OrderScreenProps
-> = (props: { userName: string; userAvatarUrl: string; numItems: number } & OrderScreenProps) => {
+	{ userAvatarUrl: string; numItems: number } & OrderScreenProps
+> = (props: { userAvatarUrl: string; numItems: number } & OrderScreenProps) => {
 	const authState = useAppSelector(selectAuthState);
-	/*
-		<Avatar size={"lg"}
-			bg={"gray.800"}
-			source={require('../../assets/images/avatar.png')}
-		>{props.userName.slice(0, 2).toUpperCase()}</Avatar>
-	*/
+
 	return (
-		<>
-			<Box style={{
-				borderColor: "blue.800",
-				borderRadius: 10,
-				borderWidth: 2,
-				marginVertical: 6,
-				marginHorizontal: 30,
-				height: 100,
-			}} width={'full'} bgColor={"primary.600"}>
-				<HStack
-					flex={1}
-					alignItems={"center"}
-				>
-					<View style={{ marginLeft: 10, marginRight: 15 }}>
-						<Avatar />
-					</View>
-					<VStack
-						space={2}
-					>
-						<Text
-							style={{
-								color: "white",
-							}}
-							fontSize={"md"}
-						>{props.userName}</Text>
-						<Text
-							style={{
-								color: "white",
-							}}
-							fontSize={"sm"}
-						>{"You have " + props.numItems + " items up to sale"}</Text>
-					</VStack>
-					<Button
-						backgroundColor={"white"}
-						style={{
-							marginLeft: 10,
-							width: 70,
-							height: 45,
-							borderRadius: 10,
-							borderWidth: 2,
-							borderColor: "black"
-						}}
-					><Text
-						color={"primary.700"}
-						bold
-						onPress={() => {
-							props.navigation.navigate('OrderCreateStack');
-							if (authState.isSignout === false && authState.userToken) {
-								/**
-								 * Move to OrderScreen with a state hook
-								 */
-								getAllItemSync(3, authState.userToken).then((list) => {
-									console.log(list);
-								})
-							}
-						}}
-					>Post</Text></Button>
-				</HStack>
-			</Box >
-		</>
+		<Box style={{
+			borderRadius: 10,
+			marginVertical: 6,
+			marginHorizontal: 30,
+			height: 100,
+		}} width={'full'} bgColor={"primary.600"}>
+			<HStack flex={1} alignItems={"center"}>
+				<View style={{ marginLeft: 10, marginRight: 15 }}>
+					<Avatar />
+				</View>
+				<VStack space={2}>
+					<Text style={{ color: "white" }} fontSize={"lg"}>{authState.userName}</Text>
+					<Text style={{ color: "white" }} fontSize={"sm"}>{"You have " + props.numItems + " items up to sale"}</Text>
+				</VStack>
+				<Button
+					backgroundColor={"white"}
+					style={{
+						marginLeft: 15,
+						width: 70,
+						height: 45,
+						borderRadius: 10,
+					}}
+					onPress={() => {
+						props.navigation.navigate('OrderCreateStack');
+					}}
+				><Text color={"primary.700"} bold>Post</Text>
+				</Button>
+			</HStack>
+		</Box >
 	)
+}
+
+const ProfileNotLogin: React.FunctionComponent<OrderScreenProps> = (props: OrderScreenProps) => {
+	return (
+		<Box style={{
+			borderRadius: 10,
+			marginVertical: 6,
+			marginHorizontal: 30,
+			height: 100,
+		}} width={'full'} bgColor={"primary.600"}>
+			<Center
+				flex={"1"}
+				alignItems={"center"}
+				justifyContent={"center"}
+			>
+				<Text style={{ color: "white" }} fontSize={"lg"}>Please login to check your orders.</Text>
+			</Center>
+		</Box >
+	)
+}
+
+const OrderList: React.FunctionComponent<{ items: Item.Item[] } & OrderScreenProps> = (
+	props: { items: Item.Item[] } & OrderScreenProps
+) => {
+	const data = props.items;
+	return (
+		<FlatList
+			width={"full"}
+			borderColor={"gray.400"}
+			style={{
+				borderBottomWidth: 0,
+				borderRightWidth: 1,
+				borderLeftWidth: 1,
+				borderTopWidth: 1,
+				borderTopLeftRadius: 20,
+				borderTopRightRadius: 20,
+			}}
+			data={data}
+			keyExtractor={item => item.id.toString()}
+			renderItem={({ item }) =>
+				<Box
+					borderBottomColor={"gray.400"}
+					style={{
+						borderBottomWidth: 1,
+
+						paddingHorizontal: 20,
+						paddingVertical: 10
+					}}>
+					<HStack space={[2, 3]}
+						style={{
+							flex: 1,
+							alignItems: "center"
+						}}
+					>
+						<Image
+							style={{ width: 50, height: 50 }}
+							source={require('../../assets/images/product.png')}
+							alt='item'
+						/>
+						<VStack>
+							<Text color="coolGray.800" bold>
+								{item.item_name}
+							</Text>
+						</VStack>
+						<Spacer />
+						<VStack style={{
+							flexDirection: "column",
+							alignItems: "flex-end",
+							marginRight: 10,
+						}}>
+							<Text fontSize="xs" color="coolGray.800" bold>
+								Ask price: {item.price}
+							</Text>
+							<Text fontSize="xs" color="coolGray.600">
+								Update: {moment.utc(item.updatedAt).utcOffset(480).format("LL")}
+							</Text>
+						</VStack>
+						<Icon
+							as={Ionicons}
+							name={"arrow-forward-sharp"}
+							color="coolGray.400"
+							size={6}
+							onPress={() => {
+								props.navigation.navigate('OrderDetailStack', { item });
+							}}
+						/>
+					</HStack>
+				</Box>
+			}
+		/>
+	);
 }
 
 const OrderScreen: React.FunctionComponent<OrderScreenProps> = (props: OrderScreenProps) => {
 	const authState = useAppSelector(selectAuthState);
+	const [items, setItems] = useState<Item.Item[] | undefined>(undefined)
+
+	const loadOrders = async (): Promise<void> => {
+		if (authState.userId && authState.userToken) {
+			const data = await getAllItemSync(authState.userId, authState.userToken);
+			if (data) setItems(data);
+		};
+	};
+
+	useEffect(() => {
+		if (authState.isSignout === true) {
+			setItems(undefined);
+		} else {
+			loadOrders();
+		}
+	}, [authState.isSignout]);
 
 	return (
 		<NativeBaseProvider>
-			<View
-				style={{
-					flex: 1, alignItems: "center", justifyContent: "flex-start", borderWidth: 2,
-					paddingHorizontal: 10
-				}}
-			>
-				<Profile
-					userName={authState.userName as string}
-					numItems={12}
-					userAvatarUrl="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
-					{...props}
-				/>
-				<ScrollView w={"full"}
-					style={{
-						borderColor: "red",
-						borderWidth: 2,
-					}}>
-
-				</ScrollView>
+			<View style={{
+				flex: 1, alignItems: "center", justifyContent: "flex-start", paddingHorizontal: 10
+			}}>
+				{
+					!items ? <ProfileNotLogin {...props} /> :
+						<Profile
+							numItems={items.length}
+							userAvatarUrl="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
+							{...props}
+						/>
+				}
+				{!items ? null : <OrderList {...props} items={items} />}
 			</View>
 		</NativeBaseProvider>
 
