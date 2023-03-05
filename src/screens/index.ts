233770742df77b5fbef1c5c5f9e6c4d996c94c9d
XsChen524 <43726972+XsChen524@users.noreply.github.com>
2023-03-06 00:15:07 +0800

    Add order modules

--- src/screens/index.ts
+++ src/screens/index.ts
@@ -1,6 +1,15 @@
 import SettingScreen from "./Setting/SettingScreen";
 import LoginScreen from "./Setting/LoginScreen";
 import SignupScreen from "./Setting/SignupScreen";
+import OrderCreateScreen from "./Order/OrderCreateScreen";
+import OrderScreen from "./Order/OrderScreen";
 import HomeScreen from "./HomeScreen";
 
-export { HomeScreen, SettingScreen, LoginScreen, SignupScreen };
+export {
+	HomeScreen,
+	SettingScreen,
+	LoginScreen,
+	SignupScreen,
+	OrderCreateScreen,
+	OrderScreen,
+};
