f49afa84c219f92c93daa2a8aed42aa532d290b6
XsChen524 <43726972+XsChen524@users.noreply.github.com>
2023-03-06 06:09:20 +0800

    Wrapped avatar, need to add uri

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -1,10 +1,15 @@
 import { Image, View } from 'react-native'
 
 const Avatar: React.FunctionComponent<{}> = () => {
+	/*
+		<Avatar size={"lg"}
+			bg={"gray.800"}
+			source={require('../../assets/images/avatar.png')}
+		>{props.userName.slice(0, 2).toUpperCase()}</Avatar>
+	*/
 	return (
 		<View
 			style={{
-				borderWidth: 2,
 				borderRadius: 50,
 				width: 70,
 				height: 70,
