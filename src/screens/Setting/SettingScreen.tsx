bf4e5c0aedb71bf69c6aebfb4bfbc07c91cfca67
XsChen524 <43726972+XsChen524@users.noreply.github.com>
2023-04-04 21:09:16 +0800

    Profile banner

--- src/screens/Setting/SettingScreen.tsx
+++ src/screens/Setting/SettingScreen.tsx
@@ -1,19 +1,71 @@
-import * as React from "react";
+import * as React from "react"
+import { useEffect, useState } from "react";
 import {
 	NativeBaseProvider,
 	Box,
 	FlatList,
 	Button,
 	Divider,
+	HStack,
+	VStack,
+	View,
+	Text,
 } from "native-base";
 import type { NativeStackScreenProps } from "@react-navigation/native-stack";
 import { useAppSelector, useAppDispatch } from "../../redux/hooks";
 import { selectAuthState, signOut, showAuth } from "../../redux/AuthSlice";
 import MenuItem from "../../components/MenuItem";
+import Avatar from '../../components/Avatar';
 import type { SettingStackParamList } from "../../router";
 import { authFormattedLog, deleteUserDataFromStorage, getUserDataFromStorage } from "../../utils/utils";
+import { User } from "../../services/user";
+import { getUserByIdSync } from "../../services/user/user";
+import { useIsFocused } from "@react-navigation/native";
+
 
 type IProps = NativeStackScreenProps<SettingStackParamList, 'SettingStack'>;
+
+const Profile: React.FunctionComponent<{}> = () => {
+	const [user, setUser] = useState<User | undefined>(undefined);
+	const authState = useAppSelector(selectAuthState);
+	const isFocused = useIsFocused();
+
+	useEffect(() => {
+		if (authState.userId && authState.userToken) {
+			getUserByIdSync(authState.userId, authState.userToken).then((data) => {
+				setUser(data);
+			})
+		} else {
+			setUser(undefined);
+		}
+	}, [isFocused, authState.isSignout]);
+
+	return (
+		<Box
+			mx={"2"}
+			my={"3"}
+			style={{
+				borderRadius: 10,
+				height: 100,
+			}} bgColor={"primary.600"}>
+			<HStack flex={1} alignItems={"center"}>
+				{
+					!user ?
+						<Text color={"white"} fontSize={"lg"}>Please login</Text> :
+						<>
+							<View style={{ marginLeft: 10, marginRight: 15 }}>
+								<Avatar />
+							</View>
+							<VStack space={2}>
+								<Text style={{ color: "white" }} fontSize={"lg"}>{authState.userName}</Text>
+								<Text style={{ color: "white" }} fontSize={"sm"}>Balance: {user.balance.toFixed(2)}    Rank: {user.rank}</Text>
+							</VStack>
+						</>
+				}
+			</HStack>
+		</Box >
+	)
+}
 
 /**
  * Flatlist MenuItem onclick TBD
@@ -42,7 +94,7 @@ const Settings = () => {
 		},
 	];
 	return (
-		<Box borderWidth={1} borderColor="primary.500">
+		<Box>
 			<FlatList
 				data={data}
 				renderItem={({ item }) => (
@@ -59,28 +111,46 @@ const Settings = () => {
 	);
 };
 
-const LoginButton: React.FunctionComponent<IProps> = (props: IProps) => {
+const Buttons: React.FunctionComponent<IProps> = (props: IProps) => {
 	const authState = useAppSelector(selectAuthState);
 	const dispatch = useAppDispatch();
 	return (
-		<>
+		<VStack space={"sm"} mt={"3"}>
 			<Button
+				width={"300"}
 				onPress={() => {
-					dispatch(signOut(null));
-					deleteUserDataFromStorage();
+					props.navigation.navigate("LoginStack");
 				}}
 			>
-				Sign Out
+				Login
 			</Button>
+			{
+				authState.isSignout ? undefined :
+					<Button
+						width={"300"}
+						onPress={() => {
+							dispatch(signOut(null));
+							deleteUserDataFromStorage();
+						}}
+					>
+						Sign Out
+					</Button>
+			}
 			<Button
+				variant="outline" colorScheme="primary"
+				borderWidth={"2"}
+				width={"300"}
 				onPress={() => {
 					dispatch(showAuth(null));
 					authFormattedLog(authState);
 				}}
 			>
-				showAuth
+				Show Redux in console
 			</Button>
 			<Button
+				variant="outline" colorScheme="primary"
+				borderWidth={"2"}
+				width={"300"}
 				onPress={async () => {
 					getUserDataFromStorage().then((storedUserData) => {
 						console.log("\x1B[33mData in SecureStore:\x1B[0m");
@@ -88,27 +158,21 @@ const LoginButton: React.FunctionComponent<IProps> = (props: IProps) => {
 					});
 				}}
 			>
-				getFromStorage
+				Show SecureStore in console
 			</Button>
-			<Button
-				onPress={() => {
-					props.navigation.navigate("LoginStack");
-				}}
-			>
-				LoginScreen
-			</Button>
-		</>
+		</VStack>
 	);
 };
 
 const SettingScreen: React.FunctionComponent<IProps> = (props: IProps) => {
 	return (
 		<NativeBaseProvider>
-			<Box borderColor="primary.500" borderWidth={1}>
+			<Box>
+				<Profile />
 				<Settings />
 				<Divider />
 				<Box alignItems="center">
-					<LoginButton {...props} />
+					<Buttons {...props} />
 				</Box>
 			</Box>
 		</NativeBaseProvider>
