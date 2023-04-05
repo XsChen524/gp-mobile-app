012ae1551128d57bf8cdec83bd59e49b62e2a0d1
XsChen524 <43726972+XsChen524@users.noreply.github.com>
2023-04-05 17:41:32 +0800

    Finish tx components

--- src/screens/Transaction/MyTxDetailScreen.tsx
+++ src/screens/Transaction/MyTxDetailScreen.tsx
@@ -1,13 +1,97 @@
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
-import { NativeBaseProvider, Text } from "native-base"
+import { Box, Column, Divider, NativeBaseProvider, Row, Text, Image, ScrollView } from "native-base"
 import { TransactionStackParamList } from "../../router";
+import { useEffect, useState } from "react";
+import { Item } from "../../services/item";
+import { useIsFocused } from "@react-navigation/native";
+import { getItemByIdSync } from "../../services/item/item";
+import { useAppSelector } from "../../redux/hooks";
+import { selectAuthState } from "../../redux/AuthSlice";
 
 type MyTxDetailProps = NativeStackScreenProps<TransactionStackParamList, "MyTransactionDetailStack">
+
+const TxDetailEntry: React.FunctionComponent<{
+	des: string, text: string | number | undefined
+}> = (props: { des: string, text: string | number | undefined }) => {
+	return (
+		<>
+			<Row my={"2"} alignItems={"center"} space={"lg"}>
+				<Column width={"1/2"}>
+					<Text fontSize={"md"} bold >{props.des}:</Text>
+				</Column>
+				<Column>
+					<Text>{props.text}</Text>
+				</Column>
+			</Row>
+			<Divider />
+		</>
+	)
+}
+
+const TxDetailList: React.FunctionComponent<MyTxDetailProps> = (props: MyTxDetailProps) => {
+	const authState = useAppSelector(selectAuthState);
+	const tx = props.route.params.tx;
+	const [item, setItem] = useState<Item.Item | undefined>(undefined);
+	const isFocused = useIsFocused();
+
+	useEffect(() => {
+		getItemByIdSync(tx.itemId, authState.userToken as string).then((data) => {
+			setItem(data);
+			console.log(data);
+		})
+	}, [isFocused])
+
+	return (
+		<ScrollView
+			minH={"100"}
+			width={"full"}
+		>
+			{
+				!item ? undefined :
+					<Box
+						width={"full"}
+						backgroundColor={"gray.300"}
+						alignItems={"center"}
+						justifyContent={"center"}
+						overflow={"hidden"}
+						style={{
+							height: 200
+						}}>
+						<Image
+							alt={"image"}
+							height="300"
+							width="300"
+							source={{
+								uri: item?.pic_url
+							}}
+						/>
+					</Box>
+			}
+			<TxDetailEntry des={"Transaction ID"} text={tx.txId} />
+			<TxDetailEntry des="Seller ID" text={tx.sellerId} />
+			<TxDetailEntry des="Buyer ID" text={tx.buyerId} />
+			<TxDetailEntry des="Item ID" text={tx.itemId} />
+			{
+				!item ? undefined :
+					<>
+						<TxDetailEntry des="Item Name" text={item.item_name} />
+						<TxDetailEntry des="Item Description" text={item.item_description} />
+					</>
+			}
+			<TxDetailEntry des={"Price"} text={tx.price} />
+			<TxDetailEntry des={"Order State"} text={tx.state} />
+			<TxDetailEntry des="Seller Rate" text={tx.sellerRate} />
+			<TxDetailEntry des="Buyer Rate" text={tx.buyerRate} />
+			<TxDetailEntry des="Comment" text={tx.comment} />
+			<TxDetailEntry des="Created At" text={tx.createdAt} />
+		</ScrollView>
+	)
+}
 
 const MyTxDetailScreen: React.FunctionComponent<MyTxDetailProps> = (props: MyTxDetailProps) => {
 	return (
 		<NativeBaseProvider>
-			<Text>MyTransactionDetail</Text>
+			<TxDetailList {...props} />
 		</NativeBaseProvider>
 	)
 }
--- src/screens/Transaction/MyTxsScreen.tsx
+++ src/screens/Transaction/MyTxsScreen.tsx
@@ -1,16 +1,84 @@
-import { NativeBaseProvider, Text } from "native-base"
+import { Box, FlatList, HStack, NativeBaseProvider, Text, Image, VStack, Spacer, Button } from "native-base"
 import { TransactionStackParamList } from "../../router";
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 
 type MyTxsProps = NativeStackScreenProps<TransactionStackParamList, "MyTransactionsStack">
 
-const MyTxsScreen: React.FunctionComponent<MyTxsProps> = (props: MyTxsProps) => {
+const TxList: React.FunctionComponent<MyTxsProps> = (props: MyTxsProps) => {
 	const txs = props.route.params.txs;
-	console.log(txs);
 
 	return (
+		<FlatList
+			width={"full"}
+			data={txs}
+			keyExtractor={tx => tx.txId.toString()}
+			renderItem={({ item }) =>
+				<Box
+					borderBottomColor={"gray.400"}
+					style={{
+						borderBottomWidth: 1,
+
+						paddingHorizontal: 20,
+						paddingVertical: 10
+					}}>
+					<HStack space={[2, 3]}
+						style={{
+							flex: 1,
+							alignItems: "center"
+						}}
+					>
+						<Image
+							style={{ width: 50, height: 50 }}
+							source={require('../../assets/images/product.png')}
+							alt='item'
+						/>
+						<VStack maxWidth={"120"}>
+							<Text color="coolGray.800" bold>
+								Item Id: {item.itemId}
+							</Text>
+							<Text color="coolGray.500" fontSize={"sm"}>
+								Rate: {item.buyerRate}
+							</Text>
+						</VStack>
+						<Spacer />
+						<VStack style={{
+							flexDirection: "column",
+							alignItems: "flex-end",
+							marginRight: 10,
+						}}>
+							<Text fontSize="xs" color="coolGray.800" bold>
+								Deal price: {item.price}
+							</Text>
+							<Text fontSize="xs" color={item.state === "up_for_sale" ? "green.600" : "danger.600"}>
+								{item.state === "up_for_sale" ? "For sale" : "Closed"}
+							</Text>
+						</VStack>
+						<Button
+							size={"sm"}
+							fontSize={"sm"}
+							variant="solid"
+							colorScheme="primary"
+							onPress={() => {
+								props.navigation.navigate('MyTransactionDetailStack', { tx: item });
+							}}
+						>Detail</Button>
+					</HStack>
+				</Box>
+			}
+		/>
+	)
+}
+
+const MyTxsScreen: React.FunctionComponent<MyTxsProps> = (props: MyTxsProps) => {
+	return (
 		<NativeBaseProvider>
-			<Text>MyTransactions</Text>
+			<Box
+				height={"full"}
+				alignContent={"center"}
+				justifyContent={"center"}
+			>
+				<TxList {...props} />
+			</Box>
 		</NativeBaseProvider>
 	)
 }
